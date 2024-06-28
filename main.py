411d1bf88a8098bba7e77192debc8500c22d2940
thucuyen127 <uyen91nct2016@gmail.com>
2024-06-28 08:19:43 +0700

    test

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI #import class FastAPI() từ thư viện fastapi
+from enum import Enum
+
+app = FastAPI() # gọi constructor và gán vào biến app
+
+
+@app.get("/") # giống flask, khai báo phương thức get và url
+async def root(): # do dùng ASGI nên ở đây thêm async, nếu bên thứ 3 không hỗ trợ thì bỏ async đi
+    return {"message": "Hello World"}
+
+@app.post("/")
+async def post():
+    return {"message": "hello from the post route"}
+
+
+@app.put("/")
+async def put():
+    return {"message": "hello from the put route"}
+
+@app.get("/users")
+async def list_users():
+    return {"message": "list users route"}
+
+@app.get("/users/me")
+async def get_current_user():
+    return {"Message": "this is the current user"}
+
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: str):
+    return {"user_id": user_id}
+
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
+
+
+@app.get("/foods/{food_name}")
+async def get_food(food_name: FoodEnum):
+    if food_name == FoodEnum.vegetables:
+        return {"food_name": food_name, "message": "you are healthy"}
+
+    if food_name.value == "fruits":
+        return {
+            "food_name": food_name,
+            "message": "you are still healthy, but like sweet things",
+        }
+    return {"food_name": food_name, "message": "i like chocolate milk"}
